e09b6a9611326c4b8356269407ab187856757eab
Lan Zhang <a73486lz@decmtcsaiml01.cs.man.ac.uk>
2024-12-09 18:00:38 +0000

    delete some redudant arguments in isabelle folder

--- isabelle/isabelle.py
+++ isabelle/isabelle.py
@@ -6,34 +6,24 @@ from isabelle_client import start_isabelle_server, get_isabelle_client
 class Isabelle:
     def __init__(self,
                  isabelle_name='test',
-                 port=8887,
                  log_file='server.log',
                  session_name='HOL',
-                 dirs=None,
-                 verbose=True,
-                 options=None,
                  watchdog_timeout=60):
         self.isabelle_name = isabelle_name
-        self.port = port
         self.log_file = log_file
         self.session_name = session_name
-        self.dirs = dirs if dirs is not None else ['./Isabelle2023']
-        self.verbose = verbose
-        self.options = options if options is not None else []
         self.watchdog_timeout = watchdog_timeout
         self._init_client()
         self._init_session()
 
     def _init_client(self):
         start_time = time.time()
-        server_info, _ = start_isabelle_server(name=self.isabelle_name, port=self.port, log_file=self.log_file)
+        server_info, _ = start_isabelle_server(name=self.isabelle_name, log_file=self.log_file)
         self.isabelle = get_isabelle_client(server_info)
         print(f'Isabelle server started with info: {server_info} in {time.time() - start_time:.2f}s.')
 
     def _init_session(self):
         start_time = time.time()
-        self.isabelle.session_build(session=self.session_name, dirs=self.dirs, verbose=self.verbose,
-                                    options=self.options)
         self.session_id = self.isabelle.session_start(session=self.session_name)
         print(f'Isabelle session started in {time.time() - start_time:.2f}s.')
 
